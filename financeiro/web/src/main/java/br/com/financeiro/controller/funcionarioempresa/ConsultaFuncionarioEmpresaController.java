0593d0d1b1ec8b3dce482e10adee42ead1241372
edmar.junior <edmar.junior@100602400-23513.ctis.local>
2014-06-26 14:56:43 -0300

    aaaaa

--- financeiro/web/src/main/java/br/com/financeiro/controller/funcionarioempresa/ConsultaFuncionarioEmpresaController.java
+++ financeiro/web/src/main/java/br/com/financeiro/controller/funcionarioempresa/ConsultaFuncionarioEmpresaController.java
@@ -2,10 +2,14 @@ package br.com.financeiro.controller.funcionarioempresa;
 
 import java.io.Serializable;
 
+import javax.ejb.EJB;
 import javax.enterprise.context.Conversation;
 import javax.enterprise.context.ConversationScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
+
+import br.com.entity.FuncionarioEmpresa;
+import br.com.financeiro.service.FuncionarioEmpresaService;
 
 @Named
 @ConversationScoped
@@ -16,7 +20,11 @@ public class ConsultaFuncionarioEmpresaController implements Serializable {
 	private static final long serialVersionUID = 1L;
 	@Inject
 	private Conversation conversation;
+	@EJB
+	private FuncionarioEmpresaService service;
 	
+	private FuncionarioEmpresa filtro;
+
 	public String comandoIncluir() {
 		conversation.begin();
 		return "";
@@ -28,5 +36,17 @@ public class ConsultaFuncionarioEmpresaController implements Serializable {
 	public String comandoExcluir() {
 		conversation.begin();
 		return "";
+	}
+
+	public void comandoFiltrar() {
+		service.obterFuncionarios();
+	}
+
+	public FuncionarioEmpresa getFiltro() {
+		return filtro;
+	}
+
+	public void setFiltro(FuncionarioEmpresa filtro) {
+		this.filtro = filtro;
 	}
 }
